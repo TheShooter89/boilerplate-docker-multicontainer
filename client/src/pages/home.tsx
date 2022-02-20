a80ace92c9c5a2c9fc712f70fe3e1ab1e9cd99ad
tanque <paoletti.francesco@gmail.com>
2022-02-20 23:27:59 +0100

    (#10) feature(client/routes/home): add get numbers placeholder button

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {
+    useCallback
+} from 'react';
 import {
     Link,
 } from 'react-router-dom';
+import axios from 'axios';
 
 const Home = () => {
+    const getAllNumbers = useCallback(async () => {
+        //
+        console.log('Getting all numbers...');
+    }, []);
+
     return (
         <React.Fragment>
             <section>
                 <h1>Home page</h1>
                 <Link to='/'>GO TO '/'</Link>
+            </section>
+
+            <section>
+                <button onClick={getAllNumbers}>GET ALL NUMBERS</button>
             </section>
         </React.Fragment>
     );
