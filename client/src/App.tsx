b587c8494d983c9e51be0f88ef792fee0e1341b1
tanque <paoletti.francesco@gmail.com>
2022-02-20 22:18:15 +0100

    (#8) feature(client): correctly render page routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,17 +13,15 @@ import Note from './pages/note';
 function App() {
     return (
         <React.Fragment>
-            <Routes>
-                <header>
-                    <h1>This is a multiconatiner application</h1>
-                    <Link to='/'>Home</Link>
-                    <Link to='/note'>Note</Link>
-                </header>
+            <header>
+                <h1>This is a multiconatiner application</h1>
+                <Link to='/'>Home</Link>
+                <Link to='/note'>Note</Link>
+            </header>
 
-                <section>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/note' element={<Note />} />
-                </section>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/note' element={<Note />} />
             </Routes>
         </React.Fragment>
     );
