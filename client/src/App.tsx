30a0a68b00afcacfe92dcc2de60c091827e4f56d
tanque <paoletti.francesco@gmail.com>
2022-02-27 23:19:56 +0100

    (#8) feature(client): setup basic client app with ant design ui framework

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,111 @@
-import React from 'react';
+import React, {
+    useState,
+} from 'react';
 import {
     Routes,
     Route,
     Link,
 } from 'react-router-dom';
-import logo from './logo.svg';
+import {
+    Layout,
+    Menu,
+    Image,
+    Typography,
+} from 'antd';
+import {
+    SettingOutlined,
+    FileTextOutlined,
+    DashboardOutlined,
+} from '@ant-design/icons';
+
+import logoipsum from './logoipsum.svg';
+import logoipsum_small from './logoipsum_small.svg';
 import './App.css';
 
 import Home from './pages/home';
 import Note from './pages/note';
+import Settings from './pages/settings';
+
+import AppHeader from './components/header';
+
+const {
+    Sider
+} = Layout;
+const {
+    Text,
+} = Typography;
 
 function App() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        return setIsSidebarOpen(!isSidebarOpen);
+    };
+
     return (
         <React.Fragment>
-            <header>
-                <h1>This is a multiconatiner application</h1>
-                <Link to='/'>Home</Link>
-                <Link to='/note'>Note</Link>
-            </header>
+            <Layout id='mainLayout'>
+                <Sider
+                    collapsed={!isSidebarOpen}
+                    breakpoint='lg'
+                    // uncomment this if you want to hide icons on closed sidebar
+                    //collapsedWidth='0'
+                    theme='light'
+                    trigger={null}
+                    onBreakpoint={broken => {
+                        console.log(broken);
+                    }}
+                    onCollapse={(collapsed, type) => {
+                        console.log(collapsed, type);
+                    }}
+                >
+                    <Menu mode='inline' theme='light'>
+                        <Menu.Item
+                            key='0'
+                            className={
+                                isSidebarOpen
+                                    ? ''
+                                    : 'logo-small'
+                            }
+                            onClick={toggleSidebar}
+                        >
+                            <Image
+                                preview={false}
+                                src={
+                                    isSidebarOpen
+                                        ? logoipsum
+                                        : logoipsum_small
+                                }
+                            />
+                        </Menu.Item>
+                        <Menu.Item key='1'>
+                            {
+                                isSidebarOpen
+                                    ? <Text>multicontaitanerized app</Text>
+                                    : null
+                            }
+                        </Menu.Item>
+                        <Menu.Item key='2' icon={<DashboardOutlined />}>
+                            <Link to='/'>Home</Link>
+                        </Menu.Item>
+                        <Menu.Item key='3' icon={<FileTextOutlined />}>
+                            <Link to='/note'>Note</Link>
+                        </Menu.Item>
+                        <Menu.Item key='4' icon={<SettingOutlined />}>
+                            <Link to='/settings'>Settings</Link>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
 
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/note' element={<Note />} />
-            </Routes>
+                <Layout>
+                    <AppHeader isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/note' element={<Note />} />
+                        <Route path='/settings' element={<Settings />} />
+                    </Routes>
+                </Layout>
+            </Layout>
         </React.Fragment>
     );
 }
---
+++ client/src/components/header/header.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+    Layout,
+    Image,
+    Switch,
+    Space,
+} from 'antd';
+import {
+    MenuOutlined,
+} from '@ant-design/icons';
+
+import logoipsum from '../../logoipsum.svg';
+import './header.css';
+
+const {
+    Header: AntHeader,
+} = Layout;
+
+const Header = ({isSidebarOpen = false, toggleSidebar = () => console.log('hello layout Header')}) => {
+
+    return (
+        <React.Fragment>
+            <AntHeader className='mainHeader'>
+                <Space className='header-grid'>
+                    <div className='header-grid-item sidebar-button'>
+                        <MenuOutlined onClick={toggleSidebar} />
+                    </div>
+
+                    <div className='header-grid-item title'>
+                        <Image onClick={toggleSidebar} preview={false} src={logoipsum} />
+                    </div>
+
+                    <div className='header-grid-item dark-mode-switch'>
+                        <Switch
+                            checkedChildren='dark'
+                            unCheckedChildren='light'
+                        />
+                    </div>
+                </Space>
+            </AntHeader>
+        </React.Fragment>
+    );
+}
+
+export default Header;
