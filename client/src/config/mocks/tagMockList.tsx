859ebf809a8bda6269d95ee00a616b389634ade4
tanque <paoletti.francesco@gmail.com>
2022-04-11 19:57:56 +0200

    (#29) fix(client/config): add mock tag list file

---
+++ client/src/config/mocks/tagMockList.tsx
@@ -0,0 +1,20 @@
+const MOCK_TAG_LIST = [
+    {
+        id: 0,
+        name: 'Zelensky',
+
+    },
+    {
+        id: 1,
+        name: 'Putin',
+
+    },
+    {
+        id: 2,
+        name: 'von Der Leyen',
+
+    },
+
+];
+
+export default MOCK_TAG_LIST;
