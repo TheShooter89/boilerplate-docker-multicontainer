2548021e97f1c9435b0f49f379aefd9ed13df75d
tanque <paoletti.francesco@gmail.com>
2022-04-11 21:31:57 +0200

    (#29) fix(client/config): remove default keyword from mocks named export index file

--- client/src/config/mocks/index.tsx
+++ client/src/config/mocks/index.tsx
@@ -2,7 +2,7 @@ import MockNoteCard from './noteMockCard';
 import MOCK_NOTE_LIST from './noteMockList';
 import MOCK_TAG_LIST from './tagMockList';
 
-export default {
+export {
     MockNoteCard,
     MOCK_NOTE_LIST,
     MOCK_TAG_LIST,
